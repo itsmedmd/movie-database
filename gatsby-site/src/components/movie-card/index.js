e2807106659bca988c19cada756de0abc7de0ce9
ElqBell <silenc8@gmail.com>
2020-08-26 13:14:31 +0300

    created fallbacks for when release date or image is not defined

--- gatsby-site/src/components/movie-card/index.js
+++ gatsby-site/src/components/movie-card/index.js
@@ -1,20 +1,25 @@
 import React from "react"
 
 export default function MovieCard({movie, genres}) {
+  // get genre names from genre ids
   const movieGenres = movie.genre_ids.map(id => 
     genres.find(genre => genre.id === id).name
   );
-
+  
+  // sometimes there is no release date or poster image defined
+  const movieReleaseDate = movie.release_date ? movie.release_date.split("-")[0] : "";
+  const moviePoster = movie.poster_path ? `https://image.tmdb.org/t/p/w45${movie.poster_path}` : "no-image.png";
+  
   return (
-      <div className="movie-card">
-          <img src={`https://image.tmdb.org/t/p/w45${movie.poster_path}`} alt={`${movie.title} poster`}/>
-          <div className="movie-details">
-            <span className="title">{movie.title}</span>
-            <span className="rating">Rating: <span>{movie.vote_average}</span></span>
-            <span className="year">{movie.release_date.split("-")[0]}</span>
-            <span className="genres">{movieGenres.join(", ")}</span>
-            <a className="more-info" href={`https://www.themoviedb.org/movie/${movie.id}`}>More information</a>
-          </div>
-      </div>
+    <div className="movie-card">
+        <img src={moviePoster} alt={`${movie.title} poster`}/>
+        <div className="movie-details">
+          <span className="title">{movie.title}</span>
+          <span className="rating">Rating: <span>{movie.vote_average}</span></span>
+          <span className="year">{movieReleaseDate}</span>
+          <span className="genres">{movieGenres.join(", ")}</span>
+          <a className="more-info" href={`https://www.themoviedb.org/movie/${movie.id}`}>More information</a>
+        </div>
+    </div>
   );
 }
